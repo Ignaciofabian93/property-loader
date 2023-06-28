dc0edb2e9bc41e617debf7cfaaaf98b6da647df4
Ignacio <ignacio.rodriguez@agrosat.cl>
2023-06-28 07:55:47 -0400

    url axios

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:5000";
-const baseUrl = "https://property-loader-server.vercel.app/";
+const baseUrl = "https://property-loader-server.vercel.app";
 
 const axiosUrl = axios.create({ baseURL: baseUrl });
 
