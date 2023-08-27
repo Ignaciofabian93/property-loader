0a0d619cfca84bdb1795236cdae176f212019004
Ignacio Rodríguez Rulas <ignacio@MacBook-Pro-de-Ignacio.local>
2023-08-27 19:30:17 -0400

    axios url

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const baseUrl = "http://localhost:5000";
-const baseUrl = "https://property-loader-server-7h51-dev.fl0.io/";
+const baseUrl = "https://property-loader-server-kjxr-dev.fl0.io";
 
 const axiosUrl = axios.create({ baseURL: baseUrl });
 
