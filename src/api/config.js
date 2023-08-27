a143bf580bef434a1e4a9520f9a27ac708d3b44c
Ignacio Rodríguez Rulas <ignacio@MacBook-Pro-de-Ignacio.local>
2023-08-27 14:29:20 -0400

    url

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:5000";
-const baseUrl = "https://property-loader-server.vercel.app";
+const baseUrl = "https://property-loader-server-7h51-dev.fl0.io/";
 
 const axiosUrl = axios.create({ baseURL: baseUrl });
 
